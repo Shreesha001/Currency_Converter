52276924aa706b9c2b88d74ea4e4e2171588ffd9
Shreesha Shetty <shettyshreesha552@gmail.com>
2025-02-18 03:41:20 +0530

    Initial Commit

---
+++ lib/currency_converter_material_page.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+
+class CurrencyConverterMaterialPage extends StatefulWidget {
+  const CurrencyConverterMaterialPage({super.key});
+  @override
+  State<CurrencyConverterMaterialPage> createState() =>
+      _CurrencyConverterMaterialPage();
+}
+
+class _CurrencyConverterMaterialPage
+    extends State<CurrencyConverterMaterialPage> {
+  double result = 0;
+  final TextEditingController textEditingController = TextEditingController();
+
+  void convert() {
+    result = double.parse(textEditingController.text) * 86.67;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final border = OutlineInputBorder(
+      borderSide: const BorderSide(
+        color: Colors.black,
+        width: 3,
+        style: BorderStyle.solid,
+      ),
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 0, 0, 0),
+        elevation: 0,
+        title: Center(
+          child: Text(
+            'Currency Converter',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+      backgroundColor: const Color.fromARGB(255, 34, 130, 209),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "INR ${result != 0 ? result.toStringAsFixed(3) : 0}",
+                style: TextStyle(
+                    fontSize: 50,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              TextField(
+                controller: textEditingController,
+                style: const TextStyle(color: Colors.black, fontSize: 27),
+                decoration: InputDecoration(
+                  hintText: 'Enter the amount in U.S.D',
+                  hintStyle: const TextStyle(color: Colors.black, fontSize: 17),
+                  prefixIcon: const Icon(Icons.monetization_on_outlined),
+                  prefixIconColor: Colors.black,
+                  filled: true,
+                  fillColor: Colors.white,
+                  focusedBorder: border,
+                  enabledBorder: border,
+                ),
+                keyboardType:
+                    const TextInputType.numberWithOptions(decimal: true),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              ElevatedButton(
+                onPressed: convert,
+                style: ButtonStyle(
+                  elevation: WidgetStatePropertyAll(20),
+                  backgroundColor: WidgetStatePropertyAll(Colors.black),
+                  foregroundColor: WidgetStatePropertyAll(Colors.white),
+                  minimumSize: WidgetStatePropertyAll(
+                    Size(double.infinity, 50),
+                  ),
+                  shape: WidgetStatePropertyAll(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                ),
+                child: const Text("Convert"),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
